c1249a2767c6d20ceebfc74da8b29eed7c354802
Kevin <gackevin32@gmail.com>
2026-02-24 10:51:13 +0100

    dev direction page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { QuestsComponent } from './pages/quests/quests.component';
+import { QuestFormComponent } from './pages/quest-form/quest-form.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
+
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'quests', component: QuestsComponent },
+
+  // create + edit (même écran)
+  { path: 'quests/new', component: QuestFormComponent },
+  { path: 'quests/:id/edit', component: QuestFormComponent },
+
+  { path: 'profile', component: ProfileComponent },
+
+  // 404 -> revient au dashboard
+  { path: '**', redirectTo: 'dashboard' },
+];
---
+++ src/app/pages/quest-form/quest-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quest-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './quest-form.component.html',
+  styleUrl: './quest-form.component.scss'
+})
+export class QuestFormComponent {
+
+}
